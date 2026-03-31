ee08ff2167f4f447151ad127806ec11a866dfa58
hfdev <hf.dev54@gmail.com>
2026-03-31 16:21:22 -0300

    feat: Adiciona blocos de if, elif e else para opcoes escolhidas

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ print("2. Listar restaurante")
 print("3. Ativar restaurante")
 print("4. Sair\n")
 
-op_escolhida = input("Escolha uma opção: ")
-
-
-
+op_escolhida = input("Escolha uma opção: \n")
 print(f"você escolheu a opção: {op_escolhida}!")
+
+if op_escolhida == 1:
+    print("Cadastrar restaurante")
+elif op_escolhida == 2:
+    print("Listar restaurante")
+elif op_escolhida == 3:
+    print("Ativar restaurante")
+else:
+    print("Encerrando programa...")
