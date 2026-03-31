0b6e297feffa9ed8a66305fc9eaf650ff7ed940d
hfdev <hf.dev54@gmail.com>
2026-03-30 21:12:11 -0300

    feat: Adiciona primeira variavel e concatenação dela dentro do app

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-print("Sabor Express")
+print(" \n  Sabor Express \n ")
 print("1. Cadastrar Restaurante")
 print("2. Listar restaurante")
 print("3. Ativar restaurante")
-print("4. Sair")
+print("4. Sair\n")
+
+op_escolhida = input("Escolha uma opção: ")
+print("você escolheu a opção: ", op_escolhida)
