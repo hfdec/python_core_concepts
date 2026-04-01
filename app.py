8b7ad668d0fb75ae142213137079f8ec2d5f1c55
hfdev <hf.dev54@gmail.com>
2026-04-01 17:58:24 -0300

    feat: Adiciona def para encerrar o programa, junto library de os para limpar o terminal

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os # faz import da lib para limpar o terminal...
 print(" \n  Sabor Express \n ")
 print("1. Cadastrar Restaurante")
 print("2. Listar restaurante")
@@ -8,6 +9,15 @@ op_escolhida = int(input("Escolha uma opção: \n"))
 # op_escolhida = int(op_escolhida) é valido tmb
 print(f"você escolheu a opção: {op_escolhida}!\n")
 
+
+def encerrar_programa():
+    os.system("clear")
+    print("\n\nEncerrando programa...")
+
+
+
+
+
 if op_escolhida == 1:
     print("Cadastrar restaurante")
 elif op_escolhida == 2:
@@ -15,4 +25,4 @@ elif op_escolhida == 2:
 elif op_escolhida == 3:
     print("Ativar restaurante")
 else:
-    print("Encerrando programa...")
+    encerrar_programa()
